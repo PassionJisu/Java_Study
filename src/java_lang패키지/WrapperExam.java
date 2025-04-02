cea6a26b30e359bc4541679ca0772622145a7073
Jisoo Han <hanjisu1016@naver.com>
2025-04-03 00:47:47 +0900

    first commit

---
+++ src/java_lang패키지/WrapperExam.java
@@ -0,0 +1,14 @@
+package java_lang패키지;
+
+public class WrapperExam {
+    public static void main(String[] args) {
+        int i = 5;
+        Integer i2 = new Integer(5);
+
+        Integer i3 = 5;
+
+        int i4 = i3.intValue();
+
+        int i5 = i3;
+    }
+}
